8e6af89ef847dbe90b868d4cf4b73b4b726f330d
Priyanka-Tripathi98 <priyankatripa35@gmail.com>
2026-04-09 18:44:43 +0530

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // src/App.jsx
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, HashRouter } from 'react-router-dom';
 import { ThemeProvider } from './context/ThemeContext';
 import { CartProvider } from './context/CartContext';
 import { AuthProvider } from './context/AuthContext';
@@ -25,6 +25,7 @@ import Wishlist from './pages/Wishlist';
 
 function App() {
   return (
+    <HashRouter>
     <ThemeProvider>
       <CartProvider>
         <AuthProvider>
@@ -52,6 +53,7 @@ function App() {
         </AuthProvider>
       </CartProvider>
     </ThemeProvider>
+    </HashRouter>
   );
 }
 
